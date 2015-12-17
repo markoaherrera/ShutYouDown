57704ee6ff9ac429b062c71509a77891bdf4d014
Marko Herrera <marko.herrera@xerox.com>
2015-12-17 08:36:51 -0600

    new stuff

---
+++ src/CompositionMockup/AnotherMockupPart/AnotherMockupTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MockupContracts;
+using System.ComponentModel.Composition;
+
+namespace AnotherMockupPart
+{
+    [Export(typeof(IMockupTask))]
+    public class AnotherMockupTask : IMockupTask
+    {
+        public AnotherMockupTask()
+        {
+        }
+
+        public void ExecuteMockupTask()
+        {
+            Console.WriteLine("Executting another task");
+        }
+    }
+}
